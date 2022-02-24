0dc32cda8090c781f023ef7cdb458b7319a86835
Antoine <antoine.lounis@protonmail.com>
2022-02-24 22:23:20 +0100

    Bootstrap + addon BS -> Display file name

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title>PHP 8 Upload Multiple Files Example</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.min.js"></script>
+
+    <title>PHP 8 Upload Torrent</title>
     <style>
         .container {
             max-width: 450px;
@@ -18,15 +22,18 @@
 <body>
 <div class="container mt-5">
     <form action="upload.php" method="post" enctype="multipart/form-data" class="mb-3">
-        <h3 class="text-center mb-5">Upload Multiple Images in PHP 8</h3>
+        <h3 class="text-center mb-5">Upload torrent</h3>
         <div class="user-image mb-3 text-center">
             <div class="imgGallery">
                 <!-- Image preview -->
             </div>
         </div>
-        <div class="custom-file">
+
+        <div class="custom-file mt-2">
             <input type="file" name="torrent_file[]" class="custom-file-input" id="chooseFile" multiple>
-            <label class="custom-file-label" for="chooseFile">Select file</label>
+            <label class="custom-file-label" for="chooseFile">
+                <span class="d-inline-block text-truncate w-75">Sélectionner fichiers</span>
+            </label>
         </div>
 
 <!--        --><?php
@@ -46,7 +53,7 @@
 //            echo "</select><br>";
 //        ?>
         <button type="submit" name="submit" class="btn btn-primary btn-block mt-4">
-            Upload Files
+            Upload Torrents
         </button>
     </form>
     <!-- Display response messages -->
@@ -78,5 +85,10 @@
 <!--        });-->
 <!--    });-->
 <!--</script>-->
+<script>
+    $(document).ready(function () {
+        bsCustomFileInput.init()
+    })
+</script>
 </body>
 </html>
